11787c73124431ae18f44e3138f9141c5c234e23
Fagley Ali Hossain <fagleyali@Fagleys-MacBook-Pro.local>
2019-05-08 21:21:33 -0500

    modified and added couple of functionalities

--- src/index.js
+++ src/index.js
@@ -3,32 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
+function Square (props) {
   
-    render() {
+    
       return (
         <button 
           className="square" 
-          onClick={()=>this.props.onClick()}
+          onClick={()=> props.onClick()}
         >
-          {this.props.value}
+          {props.value}
         </button>
       );
     }
-  }
+  
   
   class Board extends React.Component {
     constructor(){
       super()
       this.state={
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
+        xIsNext: true,
       }
     }
 
     handleClick=(i)=>{
       const squares = this.state.squares.slice()
-      squares[i]='X'
-      this.setState({squares:squares});
+      if (this.calculateWinner(squares)|| squares[i]){
+        return;
+      }
+      squares[i]=this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares:squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -38,9 +45,36 @@ class Square extends React.Component {
         
         />;
     }
+
+    calculateWinner=(squares)=>{
+      const lines=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6],
+        [0,4,8]
+      ]
+      for (let i=0;i<lines.length;i++){
+        const [a,b,c]=lines[i]
+        if (squares[a]&& squares[a]===squares[b]&& squares[a]=== squares[c]){
+          return squares[a];
+        }
+      }
+      return null;
+    }
   
     render() {
-      const status = 'Next player: X';
+
+      const winner = this.calculateWinner(this.state.squares)
+      let status;
+      
+      winner ? status='Winner: '+ winner
+      :
+      
+      status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
   
       return (
         <div>
